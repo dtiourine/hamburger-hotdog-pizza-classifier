19b2685c79d41b855d8091f537225c1dced54355
DTiourine <danieltiourine@gmail.com>
2024-06-21 21:51:56 -0400

    Train on VGG16

--- notebooks/model_architectures/v02.py
+++ notebooks/model_architectures/v02.py
@@ -2,71 +2,71 @@ import torch.nn as nn
 class HHP02(nn.Module):
     def __init__(self, input_shape: int,
                  hidden_units: int,
-                 output_shape: int):
+                 num_classes: int):
         super(HHP02, self).__init__()
         self.conv_block1 = nn.Sequential(
-            nn.Conv2d(in_channels=input_shape, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=input_shape, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
         self.conv_block2 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
         self.conv_block3 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
         self.conv_block4 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
         self.conv_block5 = nn.Sequential(
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(in_features=512 * 7 * 7, out_features=4096),
+            nn.Linear(in_features=hidden_units * 7 * 7, out_features=4096),
             nn.ReLU(),
             nn.Linear(in_features=4096, out_features=4096),
             nn.ReLU(),
-            nn.Linear(in_features=4096, out_features=output_shape)
+            nn.Linear(in_features=4096, out_features=num_classes)
         )
 
     def forward(self, x):
-         x = self.conv_block1(x)
-         print(x.shape)
-         x = self.conv_block2(x)
-         print(x.shape)
-         x = self.conv_block3(x)
-         print(x.shape)
-         x = self.conv_block4(x)
-         print(x.shape)
-         x = self.conv_block5(x)
-         print(x.shape)
-         x = self.classifier(x)
-         print(x.shape)
-         return x
-        # return self.classifier(
-        #  self.conv_block5(self.conv_block4(self.conv_block3(self.conv_block2(self.conv_block1(x))))))
+         # x = self.conv_block1(x)
+         # print(x.shape)
+         # x = self.conv_block2(x)
+         # print(x.shape)
+         # x = self.conv_block3(x)
+         # print(x.shape)
+         # x = self.conv_block4(x)
+         # print(x.shape)
+         # x = self.conv_block5(x)
+         # print(x.shape)
+         # x = self.classifier(x)
+         # print(x.shape)
+         # return x
+         return self.classifier(
+          self.conv_block5(self.conv_block4(self.conv_block3(self.conv_block2(self.conv_block1(x))))))
