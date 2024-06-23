1f62eef2998afbd70c179c02a2dbf2ea835fffb3
DTiourine <danieltiourine@gmail.com>
2024-06-23 18:33:08 -0400

    Add code to train_validate function to save model with best validation score; Add saved models to gitignore

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -4,25 +4,92 @@ import torchvision.models as models
 import wandb
 from tqdm import tqdm
 
-def train_validate_model(num_epochs, train_loader, valid_loader, model, criterion, optimizer, device):
+# def train_validate_model(num_epochs, train_loader, valid_loader, model, criterion, optimizer, device):
+#     wandb.watch(model, criterion, log="all", log_freq=10)
+#
+#     best_train_accuracy = 0
+#     best_val_accuracy = 0
+#
+#     # Create a single tqdm progress bar for the entire training cycle
+#     overall_progress_bar = tqdm(total=num_epochs, desc='Overall Training Progress', position=0, leave=True)
+#
+#     for epoch in range(num_epochs):
+#         # Training phase
+#         model.train()  # Set the model to training mode
+#         running_loss = 0.0
+#         correct = 0
+#         total = 0
+#
+#         for images, labels in train_loader:
+#             images, labels = images.to(device), labels.to(device)
+#             optimizer.zero_grad()  # Zero the parameter gradients
+#             outputs = model(images)
+#             loss = criterion(outputs, labels)
+#             loss.backward()
+#             optimizer.step()
+#
+#             running_loss += loss.item()
+#             _, predicted = torch.max(outputs.data, 1)
+#             total += labels.size(0)
+#             correct += (predicted == labels).sum().item()
+#
+#             wandb.log({"train_batch_loss": loss.item()})
+#
+#         train_loss = running_loss / len(train_loader)
+#         train_accuracy = 100 * correct / total
+#         best_train_accuracy = max(best_train_accuracy, train_accuracy)
+#         wandb.log({"train_loss": train_loss, "train_accuracy": train_accuracy, "epoch": epoch + 1})
+#
+#         # Validation phase
+#         model.eval()  # Set the model to evaluation mode
+#         val_running_loss = 0.0
+#         val_correct = 0
+#         val_total = 0
+#         with torch.no_grad():  # Disable gradient calculation
+#             for images, labels in valid_loader:
+#                 images, labels = images.to(device), labels.to(device)
+#                 outputs = model(images)
+#                 loss = criterion(outputs, labels)
+#                 val_running_loss += loss.item()
+#                 _, predicted = torch.max(outputs.data, 1)
+#                 val_total += labels.size(0)
+#                 val_correct += (predicted == labels).sum().item()
+#
+#                 wandb.log({"val_batch_loss": loss.item()})
+#
+#         val_loss = val_running_loss / len(valid_loader)
+#         val_accuracy = 100 * val_correct / val_total
+#         best_val_accuracy = max(best_val_accuracy, val_accuracy)
+#         wandb.log({"val_loss": val_loss, "val_accuracy": val_accuracy, "epoch": epoch + 1})
+#
+#         # Update the overall progress bar
+#         overall_progress_bar.set_postfix({
+#             'Best Train Accuracy': f'{best_train_accuracy:.2f}%',
+#             'Current Train Accuracy': f'{train_accuracy:.2f}%',
+#             'Best Val Accuracy': f'{best_val_accuracy:.2f}%',
+#             'Current Val Accuracy': f'{val_accuracy:.2f}%'
+#         })
+#         overall_progress_bar.update(1)
+#
+#     overall_progress_bar.close()
+
+def train_validate_model(num_epochs, train_loader, valid_loader, model, criterion, optimizer, device,
+                         model_save_path):
     wandb.watch(model, criterion, log="all", log_freq=10)
 
-    best_train_accuracy = 0
     best_val_accuracy = 0
 
-    # Create a single tqdm progress bar for the entire training cycle
     overall_progress_bar = tqdm(total=num_epochs, desc='Overall Training Progress', position=0, leave=True)
 
     for epoch in range(num_epochs):
-        # Training phase
-        model.train()  # Set the model to training mode
+        model.train()
         running_loss = 0.0
         correct = 0
         total = 0
 
         for images, labels in train_loader:
             images, labels = images.to(device), labels.to(device)
-            optimizer.zero_grad()  # Zero the parameter gradients
+            optimizer.zero_grad()
             outputs = model(images)
             loss = criterion(outputs, labels)
             loss.backward()
@@ -37,15 +104,13 @@ def train_validate_model(num_epochs, train_loader, valid_loader, model, criterio
 
         train_loss = running_loss / len(train_loader)
         train_accuracy = 100 * correct / total
-        best_train_accuracy = max(best_train_accuracy, train_accuracy)
         wandb.log({"train_loss": train_loss, "train_accuracy": train_accuracy, "epoch": epoch + 1})
 
-        # Validation phase
-        model.eval()  # Set the model to evaluation mode
+        model.eval()
         val_running_loss = 0.0
         val_correct = 0
         val_total = 0
-        with torch.no_grad():  # Disable gradient calculation
+        with torch.no_grad():
             for images, labels in valid_loader:
                 images, labels = images.to(device), labels.to(device)
                 outputs = model(images)
@@ -59,14 +124,16 @@ def train_validate_model(num_epochs, train_loader, valid_loader, model, criterio
 
         val_loss = val_running_loss / len(valid_loader)
         val_accuracy = 100 * val_correct / val_total
-        best_val_accuracy = max(best_val_accuracy, val_accuracy)
         wandb.log({"val_loss": val_loss, "val_accuracy": val_accuracy, "epoch": epoch + 1})
 
-        # Update the overall progress bar
+        # Save the model if the validation accuracy is the best we've seen so far.
+        if val_accuracy > best_val_accuracy:
+            best_val_accuracy = val_accuracy
+            torch.save(model.state_dict(), model_save_path)
+
         overall_progress_bar.set_postfix({
-            'Best Train Accuracy': f'{best_train_accuracy:.2f}%',
-            'Current Train Accuracy': f'{train_accuracy:.2f}%',
             'Best Val Accuracy': f'{best_val_accuracy:.2f}%',
+            'Current Train Accuracy': f'{train_accuracy:.2f}%',
             'Current Val Accuracy': f'{val_accuracy:.2f}%'
         })
         overall_progress_bar.update(1)
